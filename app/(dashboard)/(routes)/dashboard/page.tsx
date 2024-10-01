3adc93a2ee8d22c4c5030c3603739018c118a67f
awaisnazir08 <owaisnazir2228@gmail.com>
2024-10-01 23:20:56 +0500

    set up route paths

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+    return (
+        <>
+            <div>
+                Landing Page(Protected)
+            </div>
+        </>
+    )
+}
+
+export default DashboardPage;
