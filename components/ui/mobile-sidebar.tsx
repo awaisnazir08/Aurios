245011cd51c5d06a288d6fadf93e0d7c4746d31c
awaisnazir08 <owaisnazir2228@gmail.com>
2024-10-09 02:57:46 +0500

    fix the hydration error, start work on the dashboard page

--- components/ui/mobile-sidebar.tsx
+++ components/ui/mobile-sidebar.tsx
@@ -1,20 +1,33 @@
 'use client';
 
 import { Menu } from "lucide-react";
-
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
     return (
         <Sheet>
-            <SheetTrigger>
-                <Button variant='ghost' size='icon' className="md:hidden">
-                    <Menu />
-                </Button>
+            {/* Wrap the Button with a div or span to avoid the nested button issue */}
+            <SheetTrigger asChild>
+                <div>
+                    <Button variant='ghost' size='icon' className="md:hidden">
+                        <Menu />
+                    </Button>
+                </div>
             </SheetTrigger>
-            <SheetContent side='left' className = 'p-0'>
+            <SheetContent side='left' className='p-0'>
                 <Sidebar />
             </SheetContent>
         </Sheet>
