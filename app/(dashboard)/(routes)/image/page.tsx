de650bb6c3c6b64834e0dda721e14440f8f0fa9c
awaisnazir08 <owaisnazir2228@gmail.com>
2024-10-14 15:35:26 +0500

    image generation functional

--- app/(dashboard)/(routes)/image/page.tsx
+++ app/(dashboard)/(routes)/image/page.tsx
@@ -11,12 +11,14 @@ import { cn } from "@/lib/utils";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import axios from 'axios';
-import { ImageIcon } from "lucide-react";
+import { Download, ImageIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { amountOptions, formSchema, resolutionOptions } from "./constants";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Card, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 
 const ImagePage = () => {
     const router = useRouter();
@@ -35,12 +37,12 @@ const ImagePage = () => {
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
             setImages([]);
-            console.log(values);
-            // const response = await axios.post("/api/image", values);
+            // console.log(values);
+            const response = await axios.post("/api/image", values);
 
-            // const urls = response.data.map((image: { url: string }) => image.url);
+            const urls = response.data.map((image: { url: string }) => image.url);
 
-            // setImages(urls);
+            setImages(urls);
 
             form.reset();
         }
@@ -159,8 +161,27 @@ const ImagePage = () => {
                             <Empty label="No images generated." />
                         </div>
                     )}
-                    <div>
-                        Images will be rendered here
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-8">
+                        {images.map((src) => (
+                            <Card key={src} className="rounded-lg overflow-hidden">
+                                <div className="relative aspect-square">
+                                    <Image 
+                                    alt = 'image'
+                                    fill
+                                    src = {src}
+                                    />
+                                </div>
+                                <CardFooter className="p-2">
+                                    <Button 
+                                    onClick={() => window.open(src)} 
+                                    variant="secondary" 
+                                    className="w-full"
+                                    >
+                                        <Download className="h-4 w-4 mr-2"/>
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        ))}
                     </div>
                 </div>
             </div>
