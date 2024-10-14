f8534953ab7cd7d908ca8d676d802079ebfdab89
awaisnazir08 <owaisnazir2228@gmail.com>
2024-10-14 18:20:21 +0500

    music generation api not working

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -111,9 +111,11 @@ const MusicPage = () => {
                             <Empty label="No music generated." />
                         </div>
                     )}
-                    <div>
-                        Music will be generated here.
-                    </div>
+                    {music && (
+                        <audio controls className="w-full mt-8">
+                            <source src={music}  />
+                        </audio>
+                    )}
                 </div>
             </div>
         </div>
