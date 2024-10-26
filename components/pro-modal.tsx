b82cb9dd6c69991b1a2f380ec3a7e0965476281b
awaisnazir08 <owaisnazir2228@gmail.com>
2024-10-26 22:02:45 +0500

    add a badge

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -2,6 +2,7 @@
 
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { useProModal } from "@/hooks/use-pro-modal";
+import { Badge } from "@/components/ui/badge";
 
 export const ProModal = () => {
 
@@ -13,7 +14,12 @@ export const ProModal = () => {
                 <DialogContent>
                     <DialogHeader>
                         <DialogTitle className="flex justify-center items-center flex-col gap-y-4 pb-2">
-                            Upgrade to Aurios
+                            <div className="flex items-center gap-x-2 font-bold py-1">
+                                Upgrade to Aurios
+                                <Badge className="uppercase text-sm py-1">
+                                    pro
+                                </Badge>
+                            </div>
                         </DialogTitle>
                     </DialogHeader>
                 </DialogContent>
