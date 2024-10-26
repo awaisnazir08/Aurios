74ce35df40a2cfb461dc8f30715524501f40f96d
awaisnazir08 <owaisnazir2228@gmail.com>
2024-10-26 22:48:26 +0500

    proModal pop-up add

--- components/free-counter.tsx
+++ components/free-counter.tsx
@@ -6,6 +6,7 @@ import { MAX_FREE_COUNTS } from "@/constants";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
 import { Zap } from "lucide-react";
+import { useProModal } from "@/hooks/use-pro-modal";
 
 interface freeCounterProps {
     apiLimitCount: number,
@@ -15,6 +16,7 @@ interface freeCounterProps {
 export const FreeCounter = ({
     apiLimitCount = 0
 }: freeCounterProps) => {
+    const proModal = useProModal();
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
@@ -37,7 +39,7 @@ export const FreeCounter = ({
                             value={(apiLimitCount / MAX_FREE_COUNTS) * 100}
                         />
                     </div>
-                    <Button className="w-full" variant='premium'>
+                    <Button onClick = {proModal.onOpen} className="w-full" variant='premium'>
                         Upgrade
                         <Zap className="w-4 h-4 ml-2 fill-white"/>
                     </Button>
